1dc2078bdbb9d77b4748ca16112caa9ad25af3c5
Gabsuel <gadsouza24@gmail.com>
2023-03-27 16:43:55 -0300

    commit

---
+++ Fibonacci.js
@@ -0,0 +1,19 @@
+function fibonacci(n) {
+  // Inicia a sequência de Fibonacci com 0 e 1
+  let fib = [0, 1];
+
+  // Adiciona os próximos valores na sequência até chegar em n ou ultrapassá-lo
+  while (fib[fib.length - 1] < n) {
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  }
+
+  // Verifica se n pertence à sequência
+  if (fib.includes(n)) {
+    console.log(n + " pertence à sequência de Fibonacci");
+  } else {
+    console.log(n + " não pertence à sequência de Fibonacci");
+  }
+}
+
+// Exemplo de uso
+fibonacci(13);
